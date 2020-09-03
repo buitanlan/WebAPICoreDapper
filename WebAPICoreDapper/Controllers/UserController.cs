b426259ab3c832c4aa9af827616c83bb662230b2
BuiTanLan <tanlanhcmus@gmail.com>
2020-09-03 17:43:12 +0700

    Function API

--- WebAPICoreDapper/Controllers/UserController.cs
+++ WebAPICoreDapper/Controllers/UserController.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Dapper;
