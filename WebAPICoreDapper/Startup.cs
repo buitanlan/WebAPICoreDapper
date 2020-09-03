738d17ef0eedefd678ada5511f96cf082037ec25
BuiTanLan <tanlanhcmus@gmail.com>
2020-09-03 09:18:12 +0700

    Custom IUserStore and IRoleStore

--- WebAPICoreDapper/Startup.cs
+++ WebAPICoreDapper/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Localization.Routing;
 using Microsoft.AspNetCore.Mvc;
@@ -21,6 +22,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using WebAPICoreDapper.Data;
+using WebAPICoreDapper.Models;
 using WebAPICoreDapper.Resources;
 
 namespace WebAPICoreDapper
@@ -37,6 +40,11 @@ namespace WebAPICoreDapper
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IUserStore<AppUser>, UserStore>();
+            services.AddTransient<IRoleStore<AppRole>, RoleStore>();
+
+            services.AddIdentity<AppUser, AppRole>()
+            .AddDefaultTokenProviders();
             services.AddControllers()
                 .AddNewtonsoftJson(options =>
                 {
